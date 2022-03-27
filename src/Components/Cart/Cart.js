31379fdd7d812fa1d3581fb6e1c5ea6bd2c718a4
myewebgit <mnislamctg7@gmail.com>
2022-03-27 19:43:54 +0600

    forth commit

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -29,3 +29,12 @@ const Cart = ({cart}) => {
 };
 
 export default Cart;
+
+
+// Question#01: How Does React Work?
+
+// React has its own DOM, as like as that of a browser. Whenever a component's state is changed, then the code is re rendered by DOM, and browser has to repaint each and every element on the screen. It takes a lot of mathematical and other calculations.
+// That’s why an app would have multiple state changes.
+// So browser is doing unnecessary cycling of complete DOM whereas only a single elements' state has changed. Thus makes the browser slower. And that's the reason why, React has Virtual DOM. It helps in minimizing the actual changes on browser DOM. Virtual DOM will be an exact copy of real DOM. In React, whenever a components' state changes, the changes are reflected in Virtual DOM. Thus, Virtual DOM has the updated state of the component.
+// React implements a virtual DOM that is basically a DOM representation in JavaScript. So when it needs to read or write to the DOM. It will use the virtual representation of it
+// Although React was conceived to be used in the browser, because of its design it can also be used in the server with Node.js.

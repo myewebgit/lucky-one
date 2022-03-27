2ea6745512d1ba22ef8e1a5eaff74dc7143eae6a
myewebgit <mnislamctg7@gmail.com>
2022-03-27 19:53:29 +0600

    final commit

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -5,18 +5,14 @@ const Cart = ({cart}) => {
     return (
         <div className='cart'>
             <h2>Selected Items List</h2>
-            
             <div className='cart-item'>
             
-          
-            {
+           {
                 
                 cart.map((item)=>( 
                 <h4 key={item.id}>{<img className='photo' src={item.img}></img>} {item.name}</h4>))
             }
           
-            
-            
             </div>
             <button >
                <p>Choose for me</p>
